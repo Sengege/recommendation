1fbeda3b29f177f072af663e78e3d9f043e2a0b1
Sengege <trace_tristan@126.com>
2017-05-12 14:09:16 +0800

    .get_labels

--- utils/tfidf/get_labels.py
+++ utils/tfidf/get_labels.py
@@ -18,35 +18,35 @@ def get_labels():
     else:
         return "<h1>Hello World"
     try:
-    	from bs4 import BeautifulSoup
-    	cs=Collect.query.filter_by(user_id=uid)
+        from bs4 import BeautifulSoup
+        cs = Collect.query.filter_by(user_id=uid)
 
-    	all=""
-    	all_tags=list()
-    	for tmp in cs:
-        	rid=tmp.resource_index
-        	resource=Resource.query.filter_by(resource_id=rid).first()
-        	oid=resource.objectid
-        	from bson import ObjectId
-        	data=mongodb.data.find_one({'tid': 3,"_id":ObjectId(oid)})
-        	txt = data['content']['body']
-        	soup = BeautifulSoup(txt)
-        	title = soup.find("h2", class_="question-title").text
-        	text = ""
-        	tempdiv = soup.find("div", class_="content")
-        	p = tempdiv.find_all("p")
-        	for i in p:
-                	text = text + (i.text).encode('utf-8')
+        all = ""
+        all_tags = list()
+        for tmp in cs:
+            rid = tmp.resource_index
+            resource = Resource.query.filter_by(resource_id=rid).first()
+            oid = resource.objectid
+            from bson import ObjectId
+            data = mongodb.data.find_one({'tid': 3, "_id": ObjectId(oid)})
+            txt = data['content']['body']
+            soup = BeautifulSoup(txt)
+            title = soup.find("h2", class_="question-title").text
+            text = ""
+            tempdiv = soup.find("div", class_="content")
+            p = tempdiv.find_all("p")
+            for i in p:
+                text = text + (i.text).encode('utf-8')
 
-        	import jieba.analyse
-        	#jieba.analyse.set_idf_path("/Users/sen-ele/PycharmProjects/recommendation/utils/tfidf/Cobuild.txt")
-        	#jieba.analyse.set_idf_path("/home/tristan/postgradute/mya/recommendation/utils/tfidf/Cobuild.txt")
-        	topK=2
-        	tags = jieba.analyse.extract_tags(text, topK=topK)
-        	#print type(tags)
-        	#print tags
-        	all_tags.extend(tags)
-    		print ','.join(all_tags)
-    except Exception,e:
-	print e
-    	return jsonify(all_tags)
+            import jieba.analyse
+            # jieba.analyse.set_idf_path("/Users/sen-ele/PycharmProjects/recommendation/utils/tfidf/Cobuild.txt")
+            # jieba.analyse.set_idf_path("/home/tristan/postgradute/mya/recommendation/utils/tfidf/Cobuild.txt")
+            topK = 2
+            tags = jieba.analyse.extract_tags(text, topK=topK)
+            # print type(tags)
+            # print tags
+            all_tags.extend(tags)
+            print ','.join(all_tags)
+    except Exception, e:
+        print e
+    return jsonify(all_tags)
